140ba1ebcbd818979a5d701258adac5a0208ae30
tom <51736857+tomp2@users.noreply.github.com>
2023-04-24 02:14:26 +0300

    Fix for windows, only use pty on linux

--- tasks.py
+++ tasks.py
@@ -1,36 +1,39 @@
 from invoke import task
+import platform
+
+USE_PTY: bool = platform.system() == "Linux"
 
 
 @task
 def start(ctx):
-    ctx.run("python3 src/skilltracker/index.py", pty=True)
+    ctx.run("python3 src/skilltracker/index.py", pty=USE_PTY)
 
 
 @task(aliases=("tests",))
 def test(ctx):
-    ctx.run("pytest src/skilltracker/tests", pty=True)
+    ctx.run("pytest src/skilltracker/tests", pty=USE_PTY)
 
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest src/skilltracker", pty=True)
+    ctx.run("coverage run --branch -m pytest src/skilltracker", pty=USE_PTY)
 
 
 @task(coverage)
 def coverage_report(ctx):
-    ctx.run("coverage html", pty=True)
+    ctx.run("coverage html", pty=USE_PTY)
 
 
 @task(aliases=("format",))
 def black(ctx):
-    ctx.run("black ./", pty=True)
+    ctx.run("black ./", pty=USE_PTY)
 
 
 @task
 def mypy(ctx):
-    ctx.run("mypy src/skilltracker", pty=True)
+    ctx.run("mypy src/skilltracker", pty=USE_PTY)
 
 
 @task(aliases=("lint",))
 def pylint(ctx):
-    ctx.run("pylint src/skilltracker", pty=True)
+    ctx.run("pylint src/skilltracker", pty=USE_PTY)
