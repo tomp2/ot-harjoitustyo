741f709d7d5ff2a04894f0be44947fead60bc257
tom <51736857+tomp2@users.noreply.github.com>
2023-04-24 03:16:01 +0300

    Add a way for user to delete their account

--- src/skilltracker/ui/view_main.py
+++ src/skilltracker/ui/view_main.py
@@ -5,7 +5,9 @@ from dearpygui import dearpygui as dpg
 
 from skilltracker import models
 from skilltracker.custom_types import Self
+from skilltracker.ui.utils import centered_window
 from skilltracker.ui.view import View
+from skilltracker.repositories.user_repository import get_default_user_repository
 
 
 class MainView(View):
@@ -23,6 +25,20 @@ class MainView(View):
         self._quit_callback = quit_callback
         self._log_out_callback = log_out_callback
 
+    def _delete_account_callback(self):
+        def _yes_callback():
+            get_default_user_repository().delete_user(self._user.username)
+            dpg.delete_item(popup)
+            self._log_out_callback()
+
+        with centered_window(label="Delete account?", modal=True, horizontal_scrollbar=True) as popup:
+            dpg.add_text("You won't be able to get your data back after this.")
+            dpg.add_separator()
+            dpg.add_text("Are you sure you want to delete everything?")
+            with dpg.group(horizontal=True):
+                dpg.add_button(label="Yes", width=75, callback=_yes_callback)
+                dpg.add_button(label="Cancel", width=75, callback=lambda: dpg.delete_item(popup))
+
     def create(self) -> Self:
         with dpg.window(label="Main Window", width=400, height=300) as window:
             self.window_tag = window
@@ -33,6 +49,7 @@ class MainView(View):
                     dpg.add_menu_item(label="Quit", callback=self._quit_callback)
                 with dpg.menu(label="User"):
                     dpg.add_menu_item(label="Log out", callback=self._log_out_callback)
+                    dpg.add_menu_item(label="Delete account", callback=self._delete_account_callback)
 
             with dpg.child_window(height=75, horizontal_scrollbar=True):
                 with dpg.group(horizontal=True):
